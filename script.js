a3b43e9e8bc0328f5ad84d50b473e2899285450a
yadhukrishnanb <yadhumottu@gmail.com>
2024-04-03 10:47:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+let a=5;
+// typeOf
+console.log(typeof(a));
+let b="hgj"
+console.log(typeof(b));
+
+let x=true;
+console.log(x); // true
+
+let y;
+console.log(typeof(y));
+
+let z=null;
+console.log(z);
+console.log(typeof(z));
+
+
+
+
+let d = "16" + "Volvo";
+console.log(d);
+console.log(typeof(d));
